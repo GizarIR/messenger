b0f1dd31adcb65e350b40f1fc8015783e45f7764
Gizar <gizarir@gmail.com>
2023-02-16 22:13:30 +0200

    Added handler signal post_save

--- app/serializers.py
+++ app/serializers.py
@@ -1,8 +1,18 @@
 from rest_framework import serializers
-from .models import Chat
+from .models import Chat, Message, User
 
 
 class ChatSerializer(serializers.ModelSerializer):
     class Meta:
         model = Chat
         fields = ('name', 'time_create', 'owner', 'participates')
+
+class MessageSerializer(serializers.ModelSerializer):
+    class Meta:
+        model = Message
+        fields = ('author', 'message', 'chat', 'time_create', 'time_update')
+
+class UserSerializer(serializers.ModelSerializer):
+    class Meta:
+        model = User
+        fields = ('first_name', 'avatar')
--- app/views.py
+++ app/views.py
@@ -1,15 +1,35 @@
 from django.shortcuts import render
+import logging
 
-from rest_framework import generics
+
+from rest_framework import generics, request
 from rest_framework.permissions import IsAuthenticated
 
-from .models import Chat
+# Импорты обработки сигналов
+from django.db.models.signals import post_save
+from django.dispatch import receiver
+
+from .models import Chat, User
 from .serializers import ChatSerializer
 
 
-class ChatAPILIstView(generics.ListAPIView):
+
+logging.basicConfig(level=logging.DEBUG)
+
+
+@receiver(post_save, sender=Chat)
+def add_owner_to_participates(sender, instance, created, **kwargs):
+    # logging.info(instance.pk, instance.owner, instance.participates)
+    chat = Chat.objects.get(pk=instance.pk)
+    user = User.objects.get(username=instance.owner)
+    logging.info(f'{chat} : {user}')
+    chat.participates.add(user)
+
+
+class ChatAPILIstView(generics.ListCreateAPIView):
     queryset = Chat.objects.all()
     serializer_class = ChatSerializer
+
 
 class ChatAPIUpdateView(generics.RetrieveUpdateAPIView):
     queryset = Chat.objects.all()
