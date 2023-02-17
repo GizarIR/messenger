99c87d0cba1e39beca5101080dcd51528d1fa0f0
Gizar <gizarir@gmail.com>
2023-02-17 21:38:29 +0200

    Added signals for create chat, modified structure db, added script for get data about models to test_data.shell

---
+++ app/signals.py
@@ -0,0 +1,15 @@
+from django.db.models.signals import post_save
+from django.dispatch import receiver
+
+from .models import Chat, ChatParticipant
+
+
+# Для срабатывания кода ниже его необходимо импортировать в apps.py AppConfig
+#     def ready(self):
+#         import app.signals
+@receiver(post_save, sender=Chat)
+def add_owner_to_participants(sender, instance, created, **kwargs):
+    # logging.info(instance.pk, instance.owner, instance.participates)
+    if created:
+        chat_member = ChatParticipant.objects.create(chat=instance, participant=instance.owner)
+        chat_member.save()
--- app/views.py
+++ app/views.py
@@ -5,25 +5,12 @@ import logging
 from rest_framework import generics, request
 from rest_framework.permissions import IsAuthenticated
 
-# Импорты обработки сигналов
-from django.db.models.signals import post_save
-from django.dispatch import receiver
 
-from .models import Chat, User
+from .models import Chat, User, ChatParticipant
 from .serializers import ChatSerializer
 
 
-
 logging.basicConfig(level=logging.DEBUG)
-
-
-# @receiver(post_save, sender=Chat)
-# def add_owner_to_participates(sender, instance, created, **kwargs):
-#     # logging.info(instance.pk, instance.owner, instance.participates)
-#     chat = Chat.objects.get(pk=instance.pk)
-#     user = User.objects.get(username=instance.owner)
-#     logging.info(f'{chat} : {user}')
-#     chat.participates.add(user)
 
 
 class ChatAPILIstView(generics.ListCreateAPIView):
