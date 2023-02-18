0d29059b087a0151bdf772892e82fe03993d8f4d
Gizar <gizarir@gmail.com>
2023-02-18 12:02:26 +0200

    Added permissions and default values  for Chat model

--- app/views.py
+++ app/views.py
@@ -2,9 +2,8 @@ from django.shortcuts import render
 import logging
 
 
-from rest_framework import generics, request
-from rest_framework.permissions import IsAuthenticated
-
+from rest_framework import generics, request, viewsets, mixins, permissions
+from rest_framework.permissions import IsAuthenticated, IsAuthenticatedOrReadOnly
 
 from .models import Chat, User, ChatParticipant
 from .serializers import ChatSerializer
@@ -12,15 +11,28 @@ from .serializers import ChatSerializer
 
 logging.basicConfig(level=logging.DEBUG)
 
-
-class ChatAPILIstView(generics.ListCreateAPIView):
+# Список всех чатов
+class ChatAPIListView(generics.ListCreateAPIView):
     queryset = Chat.objects.all()
     serializer_class = ChatSerializer
+    permission_classes = (IsAuthenticatedOrReadOnly,)
 
 
+# Чтение, изменение, удаление отдельной записи
 class ChatAPIUpdateView(generics.RetrieveUpdateAPIView):
     queryset = Chat.objects.all()
     serializer_class = ChatSerializer
-    permission_classes = (IsAuthenticated,)
+    permission_classes = (IsAuthenticated,) # настройка доступа
 
 
+class IsOwnerChatOrReadOnly(permissions.BasePermission):
+
+    def has_object_permission(self, request, view, obj):
+        if request.method in permissions.SAFE_METHODS:
+            return True
+        return obj.owner == request.user
+
+class ChatAPIDestroyView(generics.DestroyAPIView):
+    queryset = Chat.objects.all()
+    serializer_class = ChatSerializer
+    permission_classes = (IsAuthenticated, IsOwnerChatOrReadOnly) # настройка доступа
--- project/urls.py
+++ project/urls.py
@@ -20,8 +20,9 @@ from app.views import *
 
 urlpatterns = [
     path('admin/', admin.site.urls),
-    path('api/v1/chatlist/', ChatAPILIstView.as_view()),
-    path('api/v1/chatlist/<int:pk>/', ChatAPIUpdateView.as_view()),
+    path('api/v1/chat/', ChatAPIListView.as_view()),
+    path('api/v1/chat/<int:pk>/', ChatAPIUpdateView.as_view()),
+    path('api/v1/chat/<int:pk>/delete/', ChatAPIDestroyView.as_view()),
     path('api/v1/drf-auth/', include('rest_framework.urls')),
     path('api/v1/auth/', include('djoser.urls')),
     re_path(r'^auth/', include('djoser.urls.authtoken')),
