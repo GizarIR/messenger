c51f28a5aa52ec16d47bd25a29e0c84f00b2eed3
Gizar <gizarir@gmail.com>
2023-03-03 21:09:04 +0200

    Added chat server - it's work. Updated ChatConsumer

--- app/consumers.py
+++ app/consumers.py
@@ -1,19 +1,43 @@
 #  app/consumers.py
 import json
 
+from asgiref.sync import async_to_sync
 from channels.generic.websocket import WebsocketConsumer
 
 
 class ChatConsumer(WebsocketConsumer):
     def connect(self):
+        self.chat_name = self.scope['url_route']['kwargs']['chat_name'] # chat_name берем из routing.py
+        self. chat_group_name = f'chat_{self.chat_name}'
+
+        async_to_sync(self.channel_layer.group_add)(
+            self.chat_group_name,
+            self.channel_name
+        )
+
         self.accept()
 
     def disconnect(self, close_code):
-        pass
+        async_to_sync(self.channel_layer.group_discard)(
+            self.chat_group_name,
+            self.channel_name
+        )
 
     def receive(self, text_data):
         text_data_json = json.loads(text_data)
         message = text_data_json["message"]
 
-        self.send(text_data=json.dumps({"message": message}))
+        async_to_sync(self.channel_layer.group_send)(
+            self.chat_group_name,
+            {
+                'type': 'chat_message',
+                'message': message
+            }
+        )
 
+    def chat_message(self, event):
+        message = event['message']
+
+        self.send(text_data=json.dumps({
+            'message': message,
+        }))
--- app/routing.py
+++ app/routing.py
@@ -5,5 +5,5 @@ from django.urls import  re_path, path
 from . import consumers
 
 websocket_urlpatterns = [
-    path(r"ws/chat/lobby/", consumers.ChatConsumer.as_asgi()),
+    re_path(r"ws/chat/(?P<chat_name>\w+)/$", consumers.ChatConsumer.as_asgi()),
 ]
