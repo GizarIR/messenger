e7cce53fd8c0039124782222d61ff074db326fac
Gizar <gizarir@gmail.com>
2023-03-03 21:20:22 +0200

    Move to chat server on async mode

--- app/consumers.py
+++ app/consumers.py
@@ -2,42 +2,45 @@
 import json
 
 from asgiref.sync import async_to_sync
-from channels.generic.websocket import WebsocketConsumer
+from channels.generic.websocket import (
+    WebsocketConsumer, # синхронный сервер
+    AsyncWebsocketConsumer # ассинхронный сервер
+)
 
 
-class ChatConsumer(WebsocketConsumer):
-    def connect(self):
+class ChatConsumer(AsyncWebsocketConsumer):
+    async def connect(self):
         self.chat_name = self.scope['url_route']['kwargs']['chat_name'] # chat_name берем из routing.py
         self. chat_group_name = f'chat_{self.chat_name}'
 
-        async_to_sync(self.channel_layer.group_add)(
+        await self.channel_layer.group_add(
             self.chat_group_name,
             self.channel_name
         )
 
-        self.accept()
+        await self.accept()
 
-    def disconnect(self, close_code):
-        async_to_sync(self.channel_layer.group_discard)(
+    async def disconnect(self, close_code):
+        await self.channel_layer.group_discard(
             self.chat_group_name,
             self.channel_name
         )
 
-    def receive(self, text_data):
+    async def receive(self, text_data):
         text_data_json = json.loads(text_data)
         message = text_data_json["message"]
 
-        async_to_sync(self.channel_layer.group_send)(
+        await self.channel_layer.group_send(
             self.chat_group_name,
             {
-                'type': 'chat_message',
+                'type': 'chat_message', # имя функции описанной ниже
                 'message': message
             }
         )
 
-    def chat_message(self, event):
+    async def chat_message(self, event):
         message = event['message']
 
-        self.send(text_data=json.dumps({
+        await self.send(text_data=json.dumps({
             'message': message,
         }))
