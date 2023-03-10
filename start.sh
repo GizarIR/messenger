fc072ab0bc276b9a23768802381c45e85d2e53ca
Gizar <gizarir@gmail.com>
2023-03-10 12:32:19 +0200

    Update start.sh for Mac and Linux: added script for start redis-server

--- start.sh
+++ start.sh
@@ -4,6 +4,28 @@ cd ../
 source venv/bin/activate
 echo "Environment activated"
 cd project/
+echo "Redis Server will start up"
+
+if [[ "$OSTYPE" == "darwin"* ]]; then
+  osascript <<END
+  tell application "Terminal"
+      do script "redis-server"
+  end tell
+END
+elif [[ "$OSTYPE" == "linux-gnu"* ]]; then
+  if command -v gnome-terminal &> /dev/null; then
+    gnome-terminal --command='redis-server'
+  elif command -v xterm &> /dev/null; then
+    xterm -e "redis-server"
+  else
+    echo "Не удалось найти терминал для запуска redis-server"
+  fi
+else
+  echo "Операционная система не поддерживается"
+fi
+
 echo "Django Server will start up"
 python manage.py runserver
+
+
 
